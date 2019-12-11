a214713bc27193b7b03766bb65fbc84c825883ae
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-12-11 16:41:44 -0500

    clean up dl-checksum.sh

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,37 +1,43 @@
 #!/usr/bin/env sh
-VER=v0.16.2
+#set -x
 DIR=~/Downloads
-MIRROR=https://github.com/docker/machine/releases/download/${VER}
-
-RSHAFILE=sha256sum.txt
-LSHAFILE=$DIR/docker-machine-${VER}-sha256sum.txt
-
-if [ ! -e $LSHAFILE ];
-then
-    wget -q -O $LSHAFILE $MIRROR/$RSHAFILE
-fi
+MIRROR=https://github.com/docker/machine/releases/download
 
 dl()
 {
-    OS=$1
-    ARCH=$2
-    EXESUFFIX=${3:-}
-    PLATFORM=${OS}-${ARCH}
-    FILE=docker-machine-${PLATFORM}${EXESUFFIX}
-    URL=$MIRROR/$FILE
+    local ver=$1
+    local lshafile=$2
+    local os=$3
+    local arch=$4
+    local dotexe=${5:-}
+    local platform="${os}-${arch}"
+    local file=docker-machine-${platform}${dotexe}
+    local url=$MIRROR/$ver/$file
 
-    printf "    # %s\n" $URL
-    printf "    %s: sha256:%s\n" $PLATFORM `grep $FILE $LSHAFILE | awk '{print $1}'`
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform `grep $file $lshafile | awk '{print $1}'`
 }
 
-printf "  # %s\n" $MIRROR/$RSHAFILE
-printf "  %s:\n" $VER
-dl Darwin x86_64
-dl Linux aarch64
-dl Linux armhf
-dl Linux x86_64
-dl Windows i386 .exe
-dl Windows x86_64 .exe
+dl_ver()
+{
+    local ver=$1
+    local rshafile=sha256sum.txt
+    local lshafile=$DIR/docker-machine-${ver}-sha256sum.txt
+
+    if [ ! -e $lshafile ];
+    then
+        wget -q -O $lshafile $MIRROR/${ver}/$rshafile
+    fi
+
+    printf "  # %s\n" $MIRROR/${ver}/$rshafile
+    printf "  %s:\n" $ver
+    dl $ver $lshafile Darwin x86_64
+    dl $ver $lshafile Linux aarch64
+    dl $ver $lshafile Linux armhf
+    dl $ver $lshafile Linux x86_64
+    dl $ver $lshafile Windows i386 .exe
+    dl $ver $lshafile Windows x86_64 .exe
+}
 
 
-
+dl_ver ${1:-v0.16.2}
